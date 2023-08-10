e0729614071f39a94f4add687b146ed53618248b
jmcgehee <jmcgehee@caltech.edu>
2023-08-10 11:25:21 -0700

    Update interpolate_intensity.m
    Add additional comments and add dialog boxes for opening and saving files. Also added true/false input for writing files or not.

--- interpolate_intensity.m
+++ interpolate_intensity.m
@@ -1,4 +1,4 @@
-function [A_curve, norm_A, p, tbl, stats, m] = interpolate_intensity(filename)
+function [A_curve, norm_A, p, tbl, stats, m] = interpolate_intensity(tf_output_file)
 %INTERPOLATE_INTENSITY Interpolates the values in a measurement.
 %
 %   Inputs
@@ -15,6 +15,9 @@ function [A_curve, norm_A, p, tbl, stats, m] = interpolate_intensity(filename)
 
 % Define step size of interpolation
 x_intrp = (0:1/999:1)';
+
+% Get filename to file
+filename = get_filename;
 
 % Get names of sheets in excel file
 sheets = sheetnames(filename);
@@ -39,23 +42,31 @@ for i = 1:size(sheets, 1)
     norm_A{i,2} = zeros(size(data{i},2)/3, 1);
     
     % For each data set (assuming data is grouped in columns where each
-    % unique data set is three columns)
+    % replicate data set is three columns)
     for j = 1:size(data{i},2)/3
         % Save the values where interpolation is to occur in the first
         % column, and then every third column after that (1, 4, 7, ... etc)
         % for each data set
         intrp_data{i}(:,3*j-2) = x_intrp;
-
+        
+        % Use the trapezoid rule to find the area under the curve, or total
+        % intensity, using the first column as x and the second column as y
+        % selecting only data that is not NaN
         A_curve{i}(j,1) = trapz(data{i}{:,3*j-2}(~isnan(data{i}{:,3*j-2})),...
             data{i}{:,3*j-1}(~isnan(data{i}{:,3*j-2})));
-
+    
+        % Use the trapezoid rule to find the area under the curve, or total
+        % intensity, using the first column as x and the third column as y
+        % selecting only data that is not NaN
         A_curve{i}(j,2) = trapz(data{i}{:,3*j-2}(~isnan(data{i}{:,3*j-2})),...
             data{i}{:,3*j}(~isnan(data{i}{:,3*j-2})));
-
+        
+        % Normalize total intensity by dividing the total intensity in
+        % from column 3 by column 2
         norm_A{i,1}(j,1) = A_curve{i}(j,2) ./ A_curve{i}(j,1);
         
         % Interpolate the data, but only for data that is not NaN. This is
-        % done for the first column and second column per data set. First
+        % done for the first column and second column per replicate. First
         % column is the x or position and second column is the intenisty
         % for channel one.
         intrp_data{i}(:,3*j-1) = interp1(...
@@ -65,7 +76,7 @@ for i = 1:size(sheets, 1)
             'makima', NaN);
 
         % Interpolate the data, but only for data that is not NaN. This is
-        % done for the first column and third column per data set. First
+        % done for the first column and third column per replicate. First
         % column is the x or position and third column is the intenisty
         % for channel two.
         intrp_data{i}(:,3*j) = interp1(...
@@ -74,7 +85,9 @@ for i = 1:size(sheets, 1)
             x_intrp,...
             'makima', NaN);
     end
-
+    
+    % Create a group name or label for each normalized total intensity for
+    % statistical analysis
     norm_A{i,2} = repmat(sheets(i,1), [size(norm_A{i,1},1), 1]);
     
     % Save the x interpolated values in the mean_data. This is the same for
@@ -88,23 +101,34 @@ for i = 1:size(sheets, 1)
     % the second channel
     mean_data{i}(:,2) = mean(intrp_data{i}(:,2:3:size(data{i},2)),2);
     mean_data{i}(:,3) = mean(intrp_data{i}(:,3:3:size(data{i},2)),2);
+
+    if tf_output_file
+        % Open dialog box for saving excel file output, but only if true
+        % was inputed
+        [file, path] = uiputfile('*.xlsx');
+        filename_out = fullfile(path,file);
     
-    % Make an excel file with the outputs. Variable names are saved in the
-    % first row. Data is added to the row below. Mean data is added many
-    % columns down from the interpolated data.
-    writematrix(string(data{i}.Properties.VariableNames),'data.xlsx',...
-        'Sheet',sheets(i,1), 'Range', 'A1');
-    writematrix(intrp_data{i},'data.xlsx','Sheet',sheets(i,1),...
-        'Range', 'A2');
-    writematrix(mean_data{i},'data.xlsx','Sheet',sheets(i,1),...
-        'Range', 'Z2');
+        % Make an excel file with the outputs. Variable names are saved in
+        % the first row. Data is added to the row below. Mean data is added
+        % many columns down from the interpolated data.
+        writematrix(string(data{i}.Properties.VariableNames),...
+            filename_out, 'Sheet', sheets(i,1), 'Range', 'A1');
+        writematrix(intrp_data{i}, filename_out, 'Sheet', sheets(i,1),...
+            'Range', 'A2');
+        writematrix(mean_data{i}, filename_out, 'Sheet', sheets(i,1),...
+            'Range', 'Z2');
+    end
 end
 
+% Concatenate the normnalized total intensities and their group labels
 A = cat(1, norm_A{:,1});
 g = cat(1, norm_A{:,2});
 
-[p,tbl,stats] = anova1(A,g, 'off');
+% Perform one-way ANOVA on the normalized total intensities
+[p,tbl,stats] = anova1(A, g, 'off');
 
+% Perform a multiple comparision test, currently using Tukey's method, need
+% to update to dunnett's test
 m = multcompare(stats, 'display', 'off');
 
 % Code for plotting
@@ -126,4 +150,21 @@ m = multcompare(stats, 'display', 'off');
 % legend({'\itwt', '\itzfh1', '\itsna'}, 'Location', 'northwest', 'Fontsize', 24);
 % legend(gca, 'boxoff');
 % title('Doc2');
+end
+
+function path = get_filename
+%GET_FILENAME Gets filename using a UI.
+% 
+%   Outputs
+%       path: path to file selected
+% 
+%   Overview
+%       Saves the filename of the selected excel file.
+    
+    % Use menu to select file
+    [name,folder] = uigetfile({'*.xlsx', 'Excel files (*.xlsx)'},...
+        'Select one file', 'MultiSelect', 'off');
+    
+    % Construct full path
+    path = fullfile(folder,name);
 end
